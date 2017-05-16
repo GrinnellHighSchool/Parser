869a62c3921b72a1241fa453f1864b1167d4dee5
ilovehans10 <ilovehans10@users.noreply.github.com>
2017-05-16 08:41:52 -0500

    This update makes it work on IDLE
    Subprocess is no longer a module that is imported because it doesn't work in IDLE

--- File Finder.py
+++ File Finder.py
@@ -1,8 +1,9 @@
 import os
-#import subprocess for running commands
-import subprocess
+#improt qwd to find the user file
+import pwd
 
 def Searcher(Name):
+	#This searches for different versions of the names
 	Name = Name.lower()
 	nameList = []
 	nameList.append(Name)
@@ -15,15 +16,27 @@ def Searcher(Name):
 			thingToAppend = Name
 	nameList.append(thingToAppend)
 
-	subprocess.run("cd ~",shell = True)
-	rootDir = '.'
+	os.chdir("/Users/" + pwd.getpwuid( os.getuid() )[ 0 ])
 	fileLocations = []
-	for dirName, subdirList, fileList in os.walk(rootDir):
+	for dirName, subdirList, fileList in os.walk("."):
 		for fname in fileList:
 			if fname.lower() in nameList:
-				fileLocations.append("~"+(dirName + "/" + fname)[1:])
+				Location = "~"+(dirName + "/" + fname)[1:]
+				Location = list(Location)
+				#This makes sure that the spaces have a \ before them
+				k = 0
+				for letter in range(len(Location)-1):
+					currentLetter = Location[letter]
+					if currentLetter == " ":
+						i = letter
+						k += 1
+					elif currentLetter != " ":
+						k = 0
+					if k == 1:
+						Location.insert(i, "\\")
+				fileLocations.append("".join(Location))
+	#The returned list of files will have two backslashes before the spaces but that is because they are in a list
 	return fileLocations
-
-#Example
-#Searcher("hello")
-#returns ['./Desktop/Random/Terminal Files/hello']
+#You can import this as a module but if you just run the file it will look for "helo there.txt"
+if __name__ == "__main__":
+	print(Searcher("helo there.txt"))
